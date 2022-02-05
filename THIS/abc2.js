288e1263739f52413bca99e179838697c15b6e7f
chaynika <chaynikaarora2003@gmail.com>
2022-02-05 14:14:47 +0530

    nested -this- with bind

---
+++ THIS/abc2.js
@@ -0,0 +1,34 @@
+function fn()
+{
+    console.log(`Hi I am ${this.person}`);
+    function abc(){
+        console.log(`Hi I am ${this.person}`);
+    }
+    abc();   //not called through any object
+}
+
+let obj = {
+    person:'Chaynika',
+    func:fn
+}
+obj.func();   //in this case the upper one will print chaynika but the nested one will print undefined(eindow object)
+
+
+//*********************************************************************BIND FUNCTION************************************************************* */
+
+function fn()
+{
+    console.log(`Hi I am ${this.person}`);
+    function abc(){
+        console.log(`Hi I am ${this.person}`);
+    }
+    let ret = abc.bind(this);   //using bind function
+    ret();   //will give chaynika
+    abc();   //this will give undefined
+}
+
+let obj = {
+    person:'Chaynika',
+    func:fn
+}
+obj.func();   //both this will give 'chaynika';
